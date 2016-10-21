e1739ce5d5e8ea991611591ada5dbe5fcb484ba4
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-20 22:02:24 -0200

    Create 2CAP_2.2.1.py

---
+++ 2CAP_2.2.1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#*-*coding: utf-8 *-*
+#Autor: EdwardRamos
+#Date: 10/20/2016 U.S.A
+
+# Qual e o volume de uma esfera com raio 5?
+
+
+r = 5           #Valor do raio (5)
+pi = 3.14       #Valor de pi (3.14)
+
+r = 5 ** 3      #Valor do raio elevado a 3:  (5 ** 3) => 125
+
+v = 4 * pi * r  #Calculo v = 1570
+
+volume = v / 3       #v / 3 = 523.3 
+
+print (volume)      #523.3 Litros
+
