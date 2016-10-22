07140edf6a3a9ebb5e1650b7c3f8a5e006f4233c
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-10-21 22:28:15 -0200

    Create Are_Circle.py

---
+++ Are_Circle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import math
+radius = 5
+
+#def area(radius):
+#   a = math.pi * radius**2
+#   return a 
+#print(area(radius))
+
+#OR OU
+
+def area(radius):
+    return math.pi * radius**2
+print(area(radius))
+
